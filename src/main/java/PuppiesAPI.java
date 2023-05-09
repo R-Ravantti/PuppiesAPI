024cf889ea753036dcef5e96dae3b2d0882f520e
R-Ravantti <ravantti97@gmail.com>
2023-05-09 18:43:30 +0200

    Set up pom.xml to use Spring Boot and made a main class.

---
+++ src/main/java/PuppiesAPI.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppiesAPI {
+    private static final Logger log = LoggerFactory.getLogger(PuppiesAPI.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppiesAPI.class, args);
+    }
+}
